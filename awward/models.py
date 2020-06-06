c061df7b3325a03a1903db909fff8c8e1c7ee767
David <dgkilolo23@gmail.com>
2020-06-06 20:47:16 +0300

    add models for the project and user profile and migrates the changes to the database.

--- awward/models.py
+++ awward/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Project(models.Model):
+  '''
+  class that defines project objects
+  '''
+  title = models.CharField(max_length=30)
+  image = models.ImageField(upload_to='awward/')
+  description = models.TextField()
+  link = models.URLField()
+
+class Profile(models.Model):
+  profile_pic = models.ImageField(upload_to='awward/')
+  user = models.OneToOneField(User, on_delete = models.CASCADE)
+  bio = models.TextField()
+
+  def __str__(self):
+    return self.user.username 
